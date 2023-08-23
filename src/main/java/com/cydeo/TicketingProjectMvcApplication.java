e751ca964d92850d65d16e270a97fd597c6593c4
ziya <koltikin210@gmail.com>
2023-08-23 07:22:17 +0300

    all the ui are added ready to build backend

--- src/main/java/com/cydeo/TicketingProjectMvcApplication.java
+++ src/main/java/com/cydeo/TicketingProjectMvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingProjectMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketingProjectMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvcApplication.class, args);
+    }
 
 }
