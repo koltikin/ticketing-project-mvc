13ceba618170271c6f75b6f508d45bc63aa6f793
ziya <koltikin210@gmail.com>
2023-08-30 00:28:05 +0300

    project create functionality is done

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,41 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+
+    @Override
+    public ProjectDTO save(ProjectDTO project) {
+        if(project.getProjectStatus()==null) project.setProjectStatus(Status.OPEN);
+        return super.save(project,project.getProjectCode());
+    }
+
+    @Override
+    public ProjectDTO findById(String projectCode) {
+        return super.findById(projectCode);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String projectCode) {
+        super.deleteById(projectCode);
+
+    }
+
+    @Override
+    public void update(ProjectDTO project) {
+        super.update(project,project.getProjectCode());
+
+    }
+}
