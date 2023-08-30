83269bc06fe8193cb17b3dcb843a450e1994b06a
ziya <koltikin210@gmail.com>
2023-08-31 00:40:57 +0300

    task create feature is done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,20 +1,43 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/task")
 public class TaskController {
+    private final ProjectService projectService;
+    private final UserService userService;
+    private final TaskService taskService;
 
     @GetMapping("/create")
     public String taskCreate(Model model){
 
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees", userService.findAll());
+        model.addAttribute("tasksList", taskService.findAll());
+
         return "/task/create";
+
+    }
+
+    @PostMapping("/create")
+    public String taskCreateSave(@ModelAttribute("task") TaskDTO task){
+
+        taskService.save(task);
+
+        return "redirect:/task/create";
 
     }
 }
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,14 +1,23 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
 
-import java.util.AbstractMap;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
+@Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if (task.getTaskStatus()==null) task.setTaskStatus(Status.OPEN);
+        if (task.getAssignedDate() == null) task.setAssignedDate(LocalDate.now());
+        if (task.getId()==null) task.setId(UUID.randomUUID().getMostSignificantBits());
+
         return super.save(task,task.getId());
     }
 
